6777e9770d418c65e70ba8fa08afc3331907ad0a
joe <musketeerdt@gmail.com>
2025-03-11 15:23:16 +0800

    allow ngrok header

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,11 +11,12 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+from corsheaders.defaults import default_headers
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_HEADERS = list(default_headers) + [ "ngrok-skip-browser-warning", ]
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
