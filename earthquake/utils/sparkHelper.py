9a3a847e3551ededa66974410f3b9e873294bb1a
joe <musketeerdt@gmail.com>
2025-03-11 15:01:40 +0800

    spark single ton

--- earthquake/utils/sparkHelper.py
+++ earthquake/utils/sparkHelper.py
@@ -2,28 +2,24 @@ from pyspark.sql import SparkSession
 
 
 class SparkHive:
-    def __init__(self,appName="HiveExample"):
-        self.spark = SparkSession.builder \
-            .appName(appName) \
+    spark = SparkSession.builder \
+            .appName("earthQuake") \
             .master("spark://earthquake1:7077")\
             .enableHiveSupport() \
             .getOrCreate()
-    def __del__(self):    
-        print("@@shutting down spark!@@")
-        self.spark.stop()
     
     def getAllEarthQuakeData(self):
-        df = self.spark.sql("SELECT * FROM earthquake_record")
+        df = SparkHive.spark.sql("SELECT * FROM earthquake_record")
         # Convert Spark DataFrame to Pandas DataFrame
         pandas_df = df.toPandas()
         return pandas_df
     
     def getTotalCount(self):
-        res = self.spark.sql("SELECT count(*) FROM earthquake_record")
+        res = SparkHive.spark.sql("SELECT count(*) FROM earthquake_record")
         return res.take(1)[0]["count(1)"]
     
     def getAverageLevel(self):
-        res=self.spark.sql("SELECT AVG(level) FROM earthquake_record")
+        res= SparkHive.spark.sql("SELECT AVG(level) FROM earthquake_record")
         return res.take(1)[0]["avg(level)"]
         
         
