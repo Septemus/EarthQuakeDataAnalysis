f3b2477f1dc75f57efd02633dff9a191e1f55483
joe <musketeerdt@gmail.com>
2025-03-18 22:24:08 +0800

    some earthquake happens on ocean which belongs to no province

--- earthquake/utils/locationHelper.py
+++ earthquake/utils/locationHelper.py
@@ -3,4 +3,5 @@ def extract_province(address):
     # cpca.transform returns a Pandas DataFrame
     df = cpca.transform([address])
     # Extract the province from the first row
-    return df.iloc[0]['省']  # '省' is the column name for province
+    province = df.iloc[0]['省']  # '省' is the column name for province
+    return province if not province is None else address
