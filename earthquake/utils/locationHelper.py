8ddc0e434c1950fa2ecf4a04e8291b0413a170b8
joe <musketeerdt@gmail.com>
2025-03-27 19:51:39 +0800

    sea need suffixed with 海域

--- earthquake/utils/locationHelper.py
+++ earthquake/utils/locationHelper.py
@@ -1,5 +1,7 @@
 import cpca
 def extract_pos(address,property):
+    if(address == "渤海" or address == "黄海" or address=="南海"):
+        address+="海域"
     # cpca.transform returns a Pandas DataFrame
     df = cpca.transform([address])
     # Extract the province from the first row
