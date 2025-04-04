fa91461adaf008b0e6cdbbfe513d8250f36275ee
joe <musketeerdt@gmail.com>
2025-04-04 23:32:12 +0800

    django record model

--- earthquake/models.py
+++ earthquake/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Django_record(models.Model):
+    occurTime = models.DateTimeField()
+    logitude = models.FloatField()
+    latitude=models.FloatField()
+    depth=models.IntegerField()
+    level=models.FloatField()
+    location=models.CharField(max_length=100)
+    catagory=models.CharField(max_length=20)
