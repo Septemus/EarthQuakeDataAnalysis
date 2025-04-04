2c721cd754e9389284dfa2afc61a505cc2ef1fb8
joe <musketeerdt@gmail.com>
2025-04-04 22:21:33 +0800

    postgresql db

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,9 +78,13 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        'NAME': 'furong',
+        'USER': 'furong',
+        'PASSWORD': '271828',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
